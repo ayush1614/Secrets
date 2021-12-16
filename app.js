5aa48905cf526a80a3ebde93808beb80da575b5b
Ayush Katiyar <ayushkatiyar0000gmail.com>
2021-12-16 11:07:17 +0530

    updated the security functionality

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
-const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+// const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
-
+mongoose.connect("mongodb://localhost:27017/userDB",
+    { useNewUrlParser: true }).then(() => {
+        console.log("Connection successful with database");
+    }).catch((e) => {
+        console.log("No connection");
+    });
 //for security we are now making the schema from mongoose schema class
 const userSchema = new mongoose.Schema({
     email: String,
@@ -42,44 +49,31 @@ app.get("/secrets", function (request, response) {
 });
 
 app.post("/register", function (request, response) {
-    let username = request.body.username;
-    let password = request.body.password;
 
-    let errors = {};
-    User.findOne({ email: username }, function (err, foundUser) {
-        if(foundUser.email === username)
-        {
-            console.log('Email alredy exists') ; 
-        }
-        else
-        {
-            const newUser = new User({
-                email : username , 
-                password : md5(password)
-            }) ;
-            newUser.save();
-            console.log("data added successfully") ; 
-        }
+    bcrypt.hash(request.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: request.body.username,
+            password: hash 
+        });
+        newUser.save();
+        response.redirect("/secrets");
     });
-    // newUser.save();         // automatically encrypts the password
-    response.redirect("/register");
 });
 
 app.post("/login", function (request, response) {
     let enteredUsername = request.body.username;
-    let enteredPassword = request.body.password;
-
     // findOne automatically decrypts the password and find the data for us 
     User.findOne({ email: enteredUsername }, function (err, foundUser) {
         if (err) {
             console.log(err);
         }
         else {
-            if (foundUser.password === enteredPassword) {
-                response.redirect("secrets");
-            }
-            else
-                console.log("wrong password");
+            bcrypt.compare(request.body.password, foundUser.password, function(err, result) {
+                if(result===true)
+                {
+                    response.render("/secrets") ; 
+                }
+            });
         }
     });
 });
